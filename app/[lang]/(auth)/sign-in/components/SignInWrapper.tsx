7e75b6930ff5d76ba5876bf9f70e26a982304edf
HRAWEBDEV <hrawebdev@gmail.com>
2026-09-10 18:27:36 +0330

    remove public assets

--- app/[lang]/(auth)/sign-in/components/SignInWrapper.tsx
+++ app/[lang]/(auth)/sign-in/components/SignInWrapper.tsx
@@ -1,7 +1,6 @@
 import { type AuthDictionary } from "@/internalization/app/dictionaries/auth/dictionary";
 import SignInWithPassword from "./SignInWithPassword";
 import { FieldGroup } from "@/components/ui/field";
-import LogoShape from "@/components/LogoShape";
 
 export default function SignInWrapper({ dic }: { dic: AuthDictionary }) {
   return (
@@ -9,7 +8,6 @@ export default function SignInWrapper({ dic }: { dic: AuthDictionary }) {
       <FieldGroup>
         <div className="flex flex-col items-center gap-2 text-center">
           <div className="flex gap-2">
-            <LogoShape className="size-8" />
             <h1 className="text-2xl font-bold">{dic.signIn.welcomeback}</h1>
           </div>
           <p className="text-balance text-muted-foreground">
--- app/[lang]/(panel)/services/side-bar/components/SidebarLogo.tsx
+++ app/[lang]/(panel)/services/side-bar/components/SidebarLogo.tsx
@@ -2,7 +2,6 @@
 import { Button } from "@/components/ui/button";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { LiaHotelSolid } from "react-icons/lia";
-import LogoShape from "@/components/LogoShape";
 
 export default function SidebarHotelInfo() {
   return (
@@ -12,7 +11,6 @@ export default function SidebarHotelInfo() {
         className="w-full justify-stretch text-start p-2 h-auto bg-transparent rounded-none min-h-(--panel-header-height) opacity-0"
       >
         <div className="flex gap-2 items-center grow text-neutral-700 dark:text-neutral-400">
-          <LogoShape className="size-12" />
           <div className="grow grid">
             <h3 className="mb-0.5 truncate">مدیریت اقامتگاه</h3>
           </div>
