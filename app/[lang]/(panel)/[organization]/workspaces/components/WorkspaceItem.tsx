921da7db0cf39ab5e76304205d08cb3f4308bc42
HRAWEBDEV <hrawebdev@gmail.com>
2026-09-14 09:16:25 +0330

    add workspace item

---
+++ app/[lang]/(panel)/[organization]/workspaces/components/WorkspaceItem.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { getSettingsIcon } from "../../services/settings/utils/getSettingsIcon";
+import { useWorkspacesContext } from "../../services/workspaces/workspacesContext";
+import { Workspace } from "../services/workspacesApiActions";
+import { FaCheck } from "react-icons/fa6";
+import { useShareDictionary } from "@/services/share-dictionary/shareDictionaryContext";
+
+export default function WorkspaceItem({ workspace }: { workspace: Workspace }) {
+  const { activeWorksapce } = useWorkspacesContext();
+  const workspaceIcon = getSettingsIcon("workspace", {
+    className: "size-10",
+  });
+  const {
+    shareDictionary: {
+      components: { workspaceInfo: dic },
+    },
+  } = useShareDictionary();
+  const isActive = workspace.id === activeWorksapce.id;
+
+  return (
+    <li
+      data-active={isActive}
+      key={workspace.id}
+      className="group relative border border-border rounded-md"
+    >
+      <Button
+        variant="outline"
+        className="absolute top-1 inset-e-1 rounded-full size-9 grid place-content-center group-data-[active='true']:bg-teal-500/10 z-1"
+      >
+        <FaCheck className="size-5 text-neutral-400 dark:text-neutral-600 group-data-[active='true']:text-teal-700 dark:group-data-[active='true']:text-teal-400" />
+      </Button>
+      <Button
+        variant="outline"
+        className="w-full flex-col h-auto p-2 gpa-2 items-stretch text-start border-0 pt-6"
+      >
+        <div className="flex flex-col gap-2 items-center mb-2 pb-2 border-b border-border">
+          <div className="text-neutral-500">{workspaceIcon}</div>
+          <p className="text-center">{workspace.name}</p>
+        </div>
+        <div>
+          <div className="mb-1">
+            <span className="text-neutral-500 text-xs">
+              {dic.organizationName}:{" "}
+            </span>
+            <span className="text-neutral-700 dark:text-neutral-400">
+              {workspace.organizationName}
+            </span>
+          </div>
+        </div>
+      </Button>
+    </li>
+  );
+}
--- app/[lang]/(panel)/[organization]/workspaces/components/WorkspacesWrapper.tsx
+++ app/[lang]/(panel)/[organization]/workspaces/components/WorkspacesWrapper.tsx
@@ -1,10 +1,8 @@
 "use client";
+import { useState } from "react";
 import { useWorkspaces } from "../hooks/useWorkspaces";
-import { getSettingsIcon } from "../../services/settings/utils/getSettingsIcon";
 import { Button } from "@/components/ui/button";
 import { useShareDictionary } from "@/services/share-dictionary/shareDictionaryContext";
-import { FaCheck } from "react-icons/fa6";
-import { useWorkspacesContext } from "../../services/workspaces/workspacesContext";
 import { Field } from "@/components/ui/field";
 import {
   InputGroup,
@@ -12,13 +10,11 @@ import {
   InputGroupInput,
 } from "@/components/ui/input-group";
 import { FaSearch, FaPlus } from "react-icons/fa";
+import WorkspaceItem from "./WorkspaceItem";
 
 export default function WorkspacesWrapper() {
-  const { activeWorksapce } = useWorkspacesContext();
+  const [search, setSearch] = useState("");
   const workspacesQuery = useWorkspaces();
-  const workspaceIcon = getSettingsIcon("workspace", {
-    className: "size-10",
-  });
   const {
     shareDictionary: {
       components: { workspaceInfo: dic },
@@ -31,8 +27,10 @@ export default function WorkspacesWrapper() {
           <InputGroup className="bg-neutral-100 dark:bg-neutral-900">
             <InputGroupInput
               id="search"
-
+              type="search"
               placeholder={dic.search + " ..."}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
             <InputGroupAddon align="inline-end">
               <FaSearch className="size-4" />
@@ -46,40 +44,7 @@ export default function WorkspacesWrapper() {
       </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {workspacesQuery.data?.workspaces.map((workspace) => {
-          const isActive = workspace.id === activeWorksapce.id;
-          return (
-            <li
-              data-active={isActive}
-              key={workspace.id}
-              className="group relative border border-border rounded-md"
-            >
-              <Button
-                variant="outline"
-                className="absolute top-1 inset-e-1 rounded-full size-9 grid place-content-center group-data-[active='true']:bg-teal-500/10 z-1"
-              >
-                <FaCheck className="size-5 text-neutral-400 dark:text-neutral-600 group-data-[active='true']:text-teal-700 dark:group-data-[active='true']:text-teal-400" />
-              </Button>
-              <Button
-                variant="outline"
-                className="w-full flex-col h-auto p-2 gpa-2 items-stretch text-start border-0 pt-6"
-              >
-                <div className="flex flex-col gap-2 items-center mb-2 pb-2 border-b border-border">
-                  <div className="text-neutral-500">{workspaceIcon}</div>
-                  <p className="text-center">{workspace.name}</p>
-                </div>
-                <div>
-                  <div className="mb-1">
-                    <span className="text-neutral-500 text-xs">
-                      {dic.organizationName}:{" "}
-                    </span>
-                    <span className="text-neutral-700 dark:text-neutral-400">
-                      {workspace.organizationName}
-                    </span>
-                  </div>
-                </div>
-              </Button>
-            </li>
-          );
+          return <WorkspaceItem key={workspace.id} workspace={workspace} />;
         })}
       </ul>
     </div>
