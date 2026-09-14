be18fd218852cbb2f95b87b0ec8160f1392aa0d8
HRAWEBDEV <hrawebdev@gmail.com>
2026-09-14 08:50:40 +0330

    update workspace wrapper grid style

--- app/[lang]/(panel)/[organization]/workspaces/components/WorkspacesWrapper.tsx
+++ app/[lang]/(panel)/[organization]/workspaces/components/WorkspacesWrapper.tsx
@@ -19,7 +19,7 @@ export default function WorkspacesWrapper() {
   } = useShareDictionary();
   return (
     <div>
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(11rem,12rem))]">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-3">
         {workspacesQuery.data?.workspaces.map((workspace) => {
           const isActive = workspace.id === activeWorksapce.id;
           return (
