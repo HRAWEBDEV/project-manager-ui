5ae3835faf5991cb7d22e4ad3fa761f633a411a2
HRAWEBDEV <hrawebdev@gmail.com>
2026-09-19 14:14:31 +0330

    add use base path custom hook

---
+++ app/[lang]/(panel)/hooks/useBasePath.ts
@@ -0,0 +1,15 @@
+import { useMemo } from "react";
+import { useBaseConfig } from "@/services/base-config/baseConfigContext";
+import { useOrganizationContext } from "../services/organization/organizationContext";
+import { useWorkspacesContext } from "../[organization]/[workspace]/services/workspaces/workspacesContext";
+
+export function useBasePath() {
+  const { locale } = useBaseConfig();
+  const { activeOrganization } = useOrganizationContext();
+  const { activeWorksapce } = useWorkspacesContext();
+
+  const basePath = useMemo(() => {
+    return `/${locale}/${activeOrganization.slug}/${activeWorksapce.slug}`;
+  }, [locale, activeOrganization, activeWorksapce]);
+  return basePath;
+}
--- app/[lang]/(panel)/hooks/useGoHome.ts
+++ app/[lang]/(panel)/hooks/useGoHome.ts
@@ -1,19 +1,13 @@
 import { useCallback } from "react";
 import { useRouter } from "next/navigation";
-import { useOrganizationContext } from "../services/organization/organizationContext";
-import { useWorkspacesContext } from "../[organization]/[workspace]/services/workspaces/workspacesContext";
-import { useBaseConfig } from "@/services/base-config/baseConfigContext";
+import { useBasePath } from "./useBasePath";
 
 export function useGoHome() {
   const router = useRouter();
-  const { locale } = useBaseConfig();
-  const { activeOrganization } = useOrganizationContext();
-  const { activeWorksapce } = useWorkspacesContext();
+  const basePath = useBasePath();
 
   const goHome = useCallback(() => {
-    router.push(
-      `/${locale}/${activeOrganization.slug}/${activeWorksapce.slug}`,
-    );
-  }, [locale, router, activeOrganization, activeWorksapce]);
+    router.push(basePath);
+  }, [router, basePath]);
   return goHome;
 }
