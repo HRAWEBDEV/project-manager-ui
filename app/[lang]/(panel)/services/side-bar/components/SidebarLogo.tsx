bcd14c12dc70453c0128f9fed808fc53836af331
HRAWEBDEV <hrawebdev@gmail.com>
2026-09-19 08:09:46 +0330

    add logo color

--- app/[lang]/(panel)/services/side-bar/components/SidebarLogo.tsx
+++ app/[lang]/(panel)/services/side-bar/components/SidebarLogo.tsx
@@ -9,7 +9,7 @@ export default function SidebarHotelInfo() {
         className="w-full justify-stretch text-start p-2 h-auto bg-transparent rounded-none min-h-(--panel-header-height)"
       >
         <div className="flex gap-4 items-center grow text-neutral-700 dark:text-neutral-400">
-          <HiOutlineCubeTransparent className="size-11" />
+          <HiOutlineCubeTransparent className="size-11 text-primary" />
           <div className="grow grid">
             <h3 className="mb-0.5 truncate">-------</h3>
             <p className="text-xs text-neutral-500">
